4780fd31ddbb5661a6d73afcdb2d77789888b002
OsvinAlvarado <jossuealvarado29@gmail.com>
2026-04-10 20:35:09 -0600

    Creacion de entidades de tutor y materia

---
+++ TutoriasGratuitas/Entidades/MateriaEntity.cs
@@ -0,0 +1,11 @@
+namespace TutoriasGratuitas.Entidades
+{
+    public class MateriaEntity
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Area { get; set; }
+        public int Creditos { get; set; }
+        
+    }
+}
